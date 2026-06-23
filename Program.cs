7f84d5f37616c5a92b2ff57abc787ba701b5cba9
Yerkezhan.Bekova <y.bekova@gmail.com>
2026-06-23 19:58:54 +0500

    Homework 1
    Commit 4 files to Homework1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello!");
+{
+	Console.Write("My name is: ");
+	{
+		Console.WriteLine("Yerkezhan");
+		{
+			int age = 35;
+			Console.WriteLine("I am " + age);
+			{
+				Console.Write("Thank you, for your attention!");
+			}
+
+		}
+	}
+}
+
