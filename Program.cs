e932ca05d8f952df7a4303b663076c4b879c8afd
Yerkezhan.Bekova <y.bekova@gmail.com>
2026-06-23 20:05:45 +0500

    Homework 2
    Commit 3 files to Homework1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿const string courseName = "This is a c# base course";
+const int year2026 = 2026;
+
+
+Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+
+Console.WriteLine("Enter your age:");
+string ageInput = Console.ReadLine();
+
+Console.WriteLine("Enter your city:");
+string city = Console.ReadLine();
+
+Console.WriteLine("What's your job:");
+string job = Console.ReadLine();
+
+Console.WriteLine("What's your favourite programming language:");
+string programming = Console.ReadLine();
+
+Console.WriteLine("Some facts about you: ");
+string facts = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine(courseName);
+Console.WriteLine(year2026);
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {ageInput}");
+Console.WriteLine($"City: {city}");
+Console.WriteLine($"Job: {job}");
+Console.WriteLine($"Programming language: {programming}");
+Console.WriteLine($"Some facts about me: {facts}");
