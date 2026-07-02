7d37050d34f96509961f4a23a6081fcdc7a588fe
Yerkezhan.Bekova <y.bekova@gmail.com>
2026-07-02 19:40:37 +0500

    Homework 5 - before the review

---
+++ Homework5.cs
@@ -0,0 +1,45 @@
+﻿const int AdultAge = 18;
+const int MinimumPoints = 60;
+const int DiscountPoints = 100;
+
+Console.Write("Enter your name: ");
+string name = Console.ReadLine().Trim();
+
+Console.Write("What's your age?: ");
+int age = Convert.ToInt32(Console.ReadLine().Trim());
+
+Console.Write("How namy points you have?: ");
+int points = Convert.ToInt32(Console.ReadLine().Trim());
+
+Console.Write("Do you have a ticket? Enter true or false: ");
+bool ticket = bool.Parse(Console.ReadLine().Trim());
+
+Console.Write("Do you have a promocode? Enter true or false: ");
+bool hasPromoCode = bool.Parse(Console.ReadLine().Trim());
+
+Console.Write("What is your role: ");
+string role = Console.ReadLine().Trim().ToLower();
+
+bool isAdult = age >= AdultAge;
+bool isNotAdult = isAdult != true; 
+bool hasEnoughPoints = points >= MinimumPoints;
+bool canGetDiscount = hasPromoCode || points >= DiscountPoints;
+bool canEnter = isAdult && ticket;
+bool hasAccess = (role == "admin" || role == "moderator") && isAdult;
+bool hasNoAccess = !hasAccess;
+const string Header =
+@"========== USER CHECK ==========";
+Console.WriteLine($"\n{Header}");
+Console.WriteLine($"\nUser: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Role: {role}");
+
+
+Console.WriteLine($"\nAdult of age: {isAdult}");
+Console.WriteLine($"Has enough point: {hasEnoughPoints}");
+Console.WriteLine($"Can enter: {canEnter}"); 
+Console.WriteLine($"Can get a discount: {canGetDiscount}");
+Console.WriteLine($"Can edit: {hasAccess || hasNoAccess }");
+Console.WriteLine($"Is not of legal age: {isNotAdult}"); 
+
+
