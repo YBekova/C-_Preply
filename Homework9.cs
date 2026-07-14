72f6d9db560501fcaaaa93fe4dbb21d1442ec64b
Yerkezhan.Bekova <y.bekova@gmail.com>
2026-07-14 21:12:21 +0500

    Homework9 - calculator

---
+++ Homework9.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Enter a number a: ");
+bool a = decimal.TryParse(Console.ReadLine(), out decimal firstValue);
+
+Console.Write("Enter operator(+ - * /): ");
+string operation = Console.ReadLine();
+
+Console.Write("Enter a number b: ");
+bool b = decimal.TryParse(Console.ReadLine(), out decimal secondValue);
+switch (operation)
+{
+    case "+":
+        Console.WriteLine($"Result: {firstValue + secondValue}");
+        break;
+
+    case "-":
+        Console.WriteLine($"Result: {firstValue - secondValue}");
+        break;
+
+    case "*":
+        Console.WriteLine($"Result: {firstValue * secondValue}");
+        break;
+
+    case "/":
+        if (secondValue == 0)
+        {
+            Console.WriteLine("You can't divide by zero!");
+            break;
+        }
+        Console.WriteLine($"Result {firstValue / secondValue}");
+        break;
+
+    default:
+        Console.WriteLine("Unknown operation");
+        break;
+
+}
