d78901ab591e417ead15312b669627a10bb63d48
Yerkezhan.Bekova <y.bekova@gmail.com>
2026-07-23 14:15:20 +0500

    Homework10 - for

---
+++ Homework11.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter value: ");
+
+bool valueValid = decimal.TryParse(Console.ReadLine(), out decimal value);
+
+for(int i = 1; i <= 10; i++)
+{
+    
+    var result = Convert.ToDecimal(value * i);
+    Console.WriteLine($"{value} * {i} = {result}");
+}
