4ef0d1636dcb1125480cb8db94ab0ece7d82fba3
Yerkezhan.Bekova <y.bekova@gmail.com>
2026-08-14 11:55:04 +0500

    upd git ignore + homework rework

--- Homework17.cs
+++ Homework17.cs
@@ -1,23 +1,23 @@
 ﻿using System.Runtime.CompilerServices;
-
+const int ticketPrice = 150;
 Console.Write("Enter number of seats: ");
 var isSeatsNumberValid = int.TryParse(Console.ReadLine(), out var numberOfSeats);
-var isInvaildSeatsNumber = numberOfSeats is < 5 or > 20;
+var isInvalidSeatsNumber = numberOfSeats is < 5 or > 20;
 
 var occupiedSeats = 0;
-while (!isSeatsNumberValid || isInvaildSeatsNumber)
+while (!isSeatsNumberValid || isInvalidSeatsNumber)
 {
     if (!isSeatsNumberValid)
     {
         Console.Write("Error! Enter the valid number for seats: ");
     }
-    else if (isInvaildSeatsNumber)
+    else if (isInvalidSeatsNumber)
     {
         Console.Write("Error! The number of entered seats can't be more than 20 and less than 5, try again: ");
     }
     
     isSeatsNumberValid = int.TryParse(Console.ReadLine(), out numberOfSeats);
-    isInvaildSeatsNumber = numberOfSeats is < 5 or > 20;
+    isInvalidSeatsNumber = numberOfSeats is < 5 or > 20;
 
 
 }
@@ -30,18 +30,26 @@ Console.WriteLine();
 
 while (freeSeats > 0)
 {
+    
     for (int i = 0; i < seats.Length; i++)
     {
-        if (!seats[i]) Console.Write("[ ] ");
+        var seat = seats[i];
+        if (!seat)
+        {
+            if (i >= 9) Console.Write(" [ ] ");
+            Console.Write("[ ] ");
+        }
         
         else Console.Write("[X] ");
         
     }
-    
+
+
     Console.WriteLine();
     
     for (int i = 0; i < seats.Length; i++)
     {
+        
         Console.Write($" {i + 1}  ");
     }
 
@@ -92,21 +100,7 @@ while (freeSeats > 0)
 }
 
 
-
-var countedOccupied = 0;
-var countedFree = 0;
-
-
-foreach (bool seat in seats)
-{
-    if(seat) countedOccupied++;
-    else countedFree++;
-    
-}
-
-int cashierSum = 0;
 var occupancyPercentage = (double)occupiedSeats / numberOfSeats * 100;
-var ticketPrice = 150;
 var ticketSum = ticketPrice * occupiedSeats;
 
 Console.WriteLine($"Sold seats: {occupiedSeats} from {numberOfSeats}");
