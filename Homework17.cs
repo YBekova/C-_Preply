01e5349c9b7be25b012acf2698e3ef651406565e
RostilKant <rostilnytskiy@gmail.com>
2026-08-20 09:40:50 +0300

    [*] Teacher fixes

--- Homework17.cs
+++ Homework17.cs
@@ -1,28 +1,29 @@
-using System.Runtime.CompilerServices;
+const int ticketPrice = 150;
+const int doubleSeatsIndex = 9;
 
 Console.Write("Enter number of seats: ");
 var isSeatsNumberValid = int.TryParse(Console.ReadLine(), out var numberOfSeats);
-var isInvaildSeatsNumber = numberOfSeats is < 5 or > 20;
+var isInvalidSeatsNumber = numberOfSeats is < 5 or > 20;
 
 var occupiedSeats = 0;
-while (!isSeatsNumberValid || isInvaildSeatsNumber)
+while (!isSeatsNumberValid || isInvalidSeatsNumber)
 {
     if (!isSeatsNumberValid)
     {
         Console.Write("Error! Enter the valid number for seats: ");
     }
-    else if (isInvaildSeatsNumber)
+    else if (isInvalidSeatsNumber)
     {
         Console.Write("Error! The number of entered seats can't be more than 20 and less than 5, try again: ");
     }
     
     isSeatsNumberValid = int.TryParse(Console.ReadLine(), out numberOfSeats);
-    isInvaildSeatsNumber = numberOfSeats is < 5 or > 20;
+    isInvalidSeatsNumber = numberOfSeats is < 5 or > 50;
 
 
 }
 
-bool[] seats = new bool[numberOfSeats]; // через булеан задаем массив в который кладем колчиство свободных сидений и это значение кладем в seats который всегда будет false, если сиденье свободно
+var seats = new bool[numberOfSeats]; // через булеан задаем массив в который кладем колчиство свободных сидений и это значение кладем в seats который всегда будет false, если сиденье свободно
 var freeSeats = numberOfSeats;
 
 
@@ -34,6 +35,11 @@ while (freeSeats > 0)
     {
         if (!seats[i]) Console.Write("[ ] ");
         
+        if (!seats[i])
+        {
+            if (i >= doubleSeatsIndex) Console.Write("  [ ]  ");
+            else Console.Write("[ ] ");
+        }
         else Console.Write("[X] ");
         
     }
@@ -42,27 +48,23 @@ while (freeSeats > 0)
     
     for (int i = 0; i < seats.Length; i++)
     {
-        Console.Write($" {i + 1}  ");
+        if (i >= doubleSeatsIndex) Console.Write($"  {i + 1}   ");
+        else Console.Write($" {i + 1}  ");
     }
 
     Console.WriteLine();
     Console.Write("Enter seat number (0 - close cashier): ");
-    
-    var seatNumberValid = int.TryParse(Console.ReadLine(), out var seatNumber);
-    
 
-    while (!seatNumberValid || (seatNumber != 0 && (seatNumber < 1 || seatNumber > seats.Length)))
+    var seatNumber = 0;
+    var seatNumberValid = false;
+    var isSeatNumberOutOfRange = seatNumber < 1 || seatNumber > seats.Length;
+    
+    while (!seatNumberValid || (seatNumber != 0 && isSeatNumberOutOfRange))
     {
-
-        if (!seatNumberValid) Console.Write("Error! Enter a number: ");
-           
-        
-        else Console.Write($"There are only {seats.Length} seats. Try again: ");
-
-        
-        
         seatNumberValid = int.TryParse(Console.ReadLine(), out seatNumber);
 
+        if (!seatNumberValid) Console.Write("Error! Enter a number: ");
+        else if (isSeatNumberOutOfRange) Console.Write($"There are only {seats.Length} seats. Try again: ");
     }
 
     if (seatNumber == 0)
@@ -93,7 +95,6 @@ while (freeSeats > 0)
 
 
 var occupancyPercentage = (double)occupiedSeats / numberOfSeats * 100;
-var ticketPrice = 150;
 var ticketSum = ticketPrice * occupiedSeats;
 
 Console.WriteLine($"Sold seats: {occupiedSeats} from {numberOfSeats}");
