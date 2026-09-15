4b259d12fbe50a49e174581469e56b5023d201cb
Yerkezhan.Bekova <y.bekova@gmail.com>
2026-09-15 15:20:44 +0500

    Class homework -flowers

---
+++ Flower.cs
@@ -0,0 +1,37 @@
+﻿namespace C__Preply;
+
+public class Flower
+{
+    public string Colour;
+    public string Type;
+    public int Amount;
+    public bool Blossom;
+
+    public void IsBlossomed()
+    {
+        Blossom = true;
+        Console.WriteLine("Flower is blossomed");
+    }
+
+    public void IsNotBlossomed()
+    {
+        Blossom = false;
+        Console.WriteLine("Flower not blossomed");
+    }
+
+    public void FlowerInfo()
+    {
+        Console.WriteLine($"Type: {Type}, Colour: {Colour}, Amount: {Amount}");
+    }
+
+    public void IsReadyForSale()
+    {
+        IsBlossomed();
+        Console.WriteLine("Ready for Sale");
+    }
+
+    public void AddFlowers(int amount)
+    {
+        Amount += amount;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using C__Preply;
+ 
+var flowerSunflower = new Flower
+ {
+     Type = "Sunflower",
+     Colour = "Yellow",
+     Amount = 8
+     
+ };
+ 
+ var flowerRose = new Flower
+ {
+     Type = "Rose",
+     Colour = "White",
+     Amount = 23
+ };
+ 
+ 
+flowerRose.FlowerInfo();
+flowerRose.IsReadyForSale();
+Console.WriteLine();
+flowerSunflower.AddFlowers(10);
+flowerSunflower.FlowerInfo();
+flowerSunflower.IsNotBlossomed();
+
+ 
