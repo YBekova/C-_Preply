9ee06499950ddd973d62c45366c3edd7dd7e8d55
Yerkezhan.Bekova <y.bekova@gmail.com>
2026-09-17 19:01:25 +0500

    New homework - constructor

---
+++ Book.cs
@@ -0,0 +1,46 @@
+﻿namespace C__Preply;
+
+public class Book
+{
+    public string Title;
+    public string Author;
+    private bool isBorowed;
+    public static int BookCount;
+
+    public Book (string bookTitle, string bookAuthor)
+    {
+        Title = bookTitle;
+        Author = bookAuthor;
+        BookCount++;
+    }
+
+    public bool Borrow()
+    {
+        if (!isBorowed)
+        {
+            
+            Console.WriteLine("You can borrow the book.");
+            isBorowed = true;
+            return true;
+            
+        }
+        else
+        {
+            
+            Console.WriteLine("Book is already borrowed.");
+            return false;
+            
+        }
+        
+    }
+
+    public void ReturnBook()
+    {
+        isBorowed = false;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Title: {Title}, Author: {Author}, Is it borrowed {isBorowed}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,26 +1,13 @@
 ﻿using C__Preply;
- 
-var flowerSunflower = new Flower
- {
-     Type = "Sunflower",
-     Colour = "Yellow",
-     Amount = 8
-     
- };
- 
- var flowerRose = new Flower
- {
-     Type = "Rose",
-     Colour = "White",
-     Amount = 23
- };
- 
- 
-flowerRose.FlowerInfo();
-flowerRose.IsReadyForSale();
-Console.WriteLine();
-flowerSunflower.AddFlowers(10);
-flowerSunflower.FlowerInfo();
-flowerSunflower.IsNotBlossomed();
+
+var book1 = new Book("Idiot", "Feoder Dostoevsky");
+
+var book2 = new Book("Misery", "Stephen King");
+
+var book3 = new Book("Room", "Emma Donoghue");
+
+
+
+
 
  
